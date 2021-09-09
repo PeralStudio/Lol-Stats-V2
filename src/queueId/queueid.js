59bc16cda48b35f0fa6bd03241b92cec8005d74b
Alberto <info@peralstudio.com>
2021-09-09 23:01:15 +0200

    Update queueid.js

--- src/queueId/queueid.js
+++ src/queueId/queueid.js
@@ -8,7 +8,7 @@ export const queueId = [
     {
         "queueId": 2,
         "map": "Grieta del invocador",
-        "description": "5v5 Blind Pick games",
+        "description": "5v5 Blind Pick",
         "notes": "Deprecated in patch 7.19 in favor of queueId 430"
     },
     {
@@ -20,7 +20,7 @@ export const queueId = [
     {
         "queueId": 6,
         "map": "Grieta del invocador",
-        "description": "5v5 Ranked Premade games",
+        "description": "5v5 Ranked Premade",
         "notes": "Game mode deprecated"
     },
     {
@@ -32,31 +32,31 @@ export const queueId = [
     {
         "queueId": 8,
         "map": "Twisted Treeline",
-        "description": "3v3 Normal games",
+        "description": "3v3 Normal",
         "notes": "Deprecated in patch 7.19 in favor of queueId 460"
     },
     {
         "queueId": 9,
         "map": "Twisted Treeline",
-        "description": "3v3 Ranked Flex games",
+        "description": "3v3 Ranked Flex",
         "notes": "Deprecated in patch 7.19 in favor of queueId 470"
     },
     {
         "queueId": 14,
         "map": "Grieta del invocador",
-        "description": "5v5 Draft Pick games",
+        "description": "5v5 Draft Pick",
         "notes": "Deprecated in favor of queueId 400"
     },
     {
         "queueId": 16,
         "map": "Crystal Scar",
-        "description": "5v5 Dominion Blind Pick games",
+        "description": "5v5 Dominion Blind Pick",
         "notes": "Game mode deprecated"
     },
     {
         "queueId": 17,
         "map": "Crystal Scar",
-        "description": "5v5 Dominion Draft Pick games",
+        "description": "5v5 Dominion Draft Pick",
         "notes": "Game mode deprecated"
     },
     {
@@ -68,31 +68,31 @@ export const queueId = [
     {
         "queueId": 31,
         "map": "Grieta del invocador",
-        "description": "Co-op vs AI Intro Bot games",
+        "description": "Co-op vs AI Intro Bot",
         "notes": "Deprecated in patch 7.19 in favor of queueId 830"
     },
     {
         "queueId": 32,
         "map": "Grieta del invocador",
-        "description": "Co-op vs AI Beginner Bot games",
+        "description": "Co-op vs AI Beginner Bot",
         "notes": "Deprecated in patch 7.19 in favor of queueId 840"
     },
     {
         "queueId": 33,
         "map": "Grieta del invocador",
-        "description": "Co-op vs AI Intermediate Bot games",
+        "description": "Co-op vs AI Intermediate Bot",
         "notes": "Deprecated in patch 7.19 in favor of queueId 850"
     },
     {
         "queueId": 41,
         "map": "Twisted Treeline",
-        "description": "3v3 Ranked Team games",
+        "description": "3v3 Ranked Team",
         "notes": "Game mode deprecated"
     },
     {
         "queueId": 42,
         "map": "Grieta del invocador",
-        "description": "5v5 Ranked Team games",
+        "description": "5v5 Ranked Team",
         "notes": "Game mode deprecated"
     },
     {
@@ -104,13 +104,13 @@ export const queueId = [
     {
         "queueId": 61,
         "map": "Grieta del invocador",
-        "description": "5v5 Team Builder games",
+        "description": "5v5 Team Builder",
         "notes": "Game mode deprecated"
     },
     {
         "queueId": 65,
         "map": "Abismo de los Lamentos",
-        "description": "5v5 ARAM",
+        "description": "ARAM",
         "notes": "Deprecated in patch 7.19 in favor of queueId 450"
     },
     {
@@ -122,109 +122,109 @@ export const queueId = [
     {
         "queueId": 70,
         "map": "Grieta del invocador",
-        "description": "One for All games",
+        "description": "One for All",
         "notes": "Deprecated in patch 8.6 in favor of queueId 1020"
     },
     {
         "queueId": 72,
         "map": "Abismo de los Lamentos",
-        "description": "1v1 Snowdown Showdown games",
+        "description": "1v1 Snowdown Showdown",
         "notes": null
     },
     {
         "queueId": 73,
         "map": "Abismo de los Lamentos",
-        "description": "2v2 Snowdown Showdown games",
+        "description": "2v2 Snowdown Showdown",
         "notes": null
     },
     {
         "queueId": 75,
         "map": "Grieta del invocador",
-        "description": "6v6 Hexakill games",
+        "description": "6v6 Hexakill",
         "notes": null
     },
     {
         "queueId": 76,
         "map": "Grieta del invocador",
-        "description": "Ultra Rapid Fire games",
+        "description": "Ultra Rapid Fire",
         "notes": null
     },
     {
         "queueId": 78,
         "map": "Abismo de los Lamentos",
-        "description": "One For All: Mirror Mode games",
+        "description": "One For All: Mirror Mode",
         "notes": null
     },
     {
         "queueId": 83,
         "map": "Grieta del invocador",
-        "description": "Co-op vs AI Ultra Rapid Fire games",
+        "description": "Co-op vs AI Ultra Rapid Fire",
         "notes": null
     },
     {
         "queueId": 91,
         "map": "Grieta del invocador",
-        "description": "Doom Bots Rank 1 games",
+        "description": "Doom Bots Rank 1",
         "notes": "Deprecated in patch 7.19 in favor of queueId 950"
     },
     {
         "queueId": 92,
         "map": "Grieta del invocador",
-        "description": "Doom Bots Rank 2 games",
+        "description": "Doom Bots Rank 2",
         "notes": "Deprecated in patch 7.19 in favor of queueId 950"
     },
     {
         "queueId": 93,
         "map": "Grieta del invocador",
-        "description": "Doom Bots Rank 5 games",
+        "description": "Doom Bots Rank 5",
         "notes": "Deprecated in patch 7.19 in favor of queueId 950"
     },
     {
         "queueId": 96,
         "map": "Crystal Scar",
-        "description": "Ascension games",
+        "description": "Ascension",
         "notes": "Deprecated in patch 7.19 in favor of queueId 910"
     },
     {
         "queueId": 98,
         "map": "Twisted Treeline",
-        "description": "6v6 Hexakill games",
+        "description": "6v6 Hexakill",
         "notes": null
     },
     {
         "queueId": 100,
         "map": "Butcher's Bridge",
-        "description": "5v5 ARAM",
+        "description": "ARAM",
         "notes": null
     },
     {
         "queueId": 300,
         "map": "Abismo de los Lamentos",
-        "description": "Legend of the Poro King games",
+        "description": "Legend of the Poro King",
         "notes": "Deprecated in patch 7.19 in favor of queueId 920"
     },
     {
         "queueId": 310,
         "map": "Grieta del invocador",
-        "description": "Nemesis games",
+        "description": "Nemesis",
         "notes": null
     },
     {
         "queueId": 313,
         "map": "Grieta del invocador",
-        "description": "Black Market Brawlers games",
+        "description": "Black Market Brawlers",
         "notes": null
     },
     {
         "queueId": 315,
         "map": "Grieta del invocador",
-        "description": "Nexus Siege games",
+        "description": "Nexus Siege",
         "notes": "Deprecated in patch 7.19 in favor of queueId 940"
     },
     {
         "queueId": 317,
         "map": "Crystal Scar",
-        "description": "Definitely Not Dominion games",
+        "description": "Definitely Not Dominion",
         "notes": null
     },
     {
@@ -236,19 +236,19 @@ export const queueId = [
     {
         "queueId": 325,
         "map": "Grieta del invocador",
-        "description": "All Random games",
+        "description": "All Random",
         "notes": null
     },
     {
         "queueId": 400,
         "map": "Grieta del invocador",
-        "description": "5v5 Draft Pick games",
+        "description": "5v5 Normal",
         "notes": null
     },
     {
         "queueId": 410,
         "map": "Grieta del invocador",
-        "description": "5v5 Ranked Dynamic games",
+        "description": "5v5 Ranked Dynamic",
         "notes": "Game mode deprecated in patch 6.22"
     },
     {
@@ -260,7 +260,7 @@ export const queueId = [
     {
         "queueId": 430,
         "map": "Grieta del invocador",
-        "description": "5v5 Blind Pick games",
+        "description": "5v5 Blind Pick",
         "notes": null
     },
     {
@@ -272,211 +272,211 @@ export const queueId = [
     {
         "queueId": 450,
         "map": "Abismo de los Lamentos",
-        "description": "5v5 ARAM",
+        "description": "ARAM",
         "notes": null
     },
     {
         "queueId": 460,
         "map": "Twisted Treeline",
-        "description": "3v3 Blind Pick games",
+        "description": "3v3 Blind Pick",
         "notes": "Deprecated in patch 9.23"
     },
     {
         "queueId": 470,
         "map": "Twisted Treeline",
-        "description": "3v3 Ranked Flex games",
+        "description": "3v3 Ranked Flex",
         "notes": "Deprecated in patch 9.23"
     },
     {
         "queueId": 600,
         "map": "Grieta del invocador",
-        "description": "Blood Hunt Assassin games",
+        "description": "Blood Hunt Assassin",
         "notes": null
     },
     {
         "queueId": 610,
         "map": "Cosmic Ruins",
-        "description": "Dark Star: Singularity games",
+        "description": "Dark Star: Singularity",
         "notes": null
     },
     {
         "queueId": 700,
         "map": "Grieta del invocador",
-        "description": "Clash games",
+        "description": "Clash",
         "notes": null
     },
     {
         "queueId": 800,
         "map": "Twisted Treeline",
-        "description": "Co-op vs. AI Intermediate Bot games",
+        "description": "Co-op vs. AI Intermediate Bot",
         "notes": "Deprecated in patch 9.23"
     },
     {
         "queueId": 810,
         "map": "Twisted Treeline",
-        "description": "Co-op vs. AI Intro Bot games",
+        "description": "Co-op vs. AI Intro Bot",
         "notes": "Deprecated in patch 9.23"
     },
     {
         "queueId": 820,
         "map": "Twisted Treeline",
-        "description": "Co-op vs. AI Beginner Bot games",
+        "description": "Co-op vs. AI Beginner Bot",
         "notes": null
     },
     {
         "queueId": 830,
         "map": "Grieta del invocador",
-        "description": "Co-op vs. AI Intro Bot games",
+        "description": "Co-op vs. AI Intro Bot",
         "notes": null
     },
     {
         "queueId": 840,
         "map": "Grieta del invocador",
-        "description": "Co-op vs. AI Beginner Bot games",
+        "description": "Co-op vs. AI Beginner Bot",
         "notes": null
     },
     {
         "queueId": 850,
         "map": "Grieta del invocador",
-        "description": "Co-op vs. AI Intermediate Bot games",
+        "description": "Co-op vs. AI Intermediate Bot",
         "notes": null
     },
     {
         "queueId": 900,
         "map": "Grieta del invocador",
-        "description": "URF games",
+        "description": "URF",
         "notes": null
     },
     {
         "queueId": 910,
         "map": "Crystal Scar",
-        "description": "Ascension games",
+        "description": "Ascension",
         "notes": null
     },
     {
         "queueId": 920,
         "map": "Abismo de los Lamentos",
-        "description": "Legend of the Poro King games",
+        "description": "Legend of the Poro King",
         "notes": null
     },
     {
         "queueId": 940,
         "map": "Grieta del invocador",
-        "description": "Nexus Siege games",
+        "description": "Nexus Siege",
         "notes": null
     },
     {
         "queueId": 950,
         "map": "Grieta del invocador",
-        "description": "Doom Bots Voting games",
+        "description": "Doom Bots Voting",
         "notes": null
     },
     {
         "queueId": 960,
         "map": "Grieta del invocador",
-        "description": "Doom Bots Standard games",
+        "description": "Doom Bots Standard",
         "notes": null
     },
     {
         "queueId": 980,
         "map": "Valoran City Park",
-        "description": "Star Guardian Invasion: Normal games",
+        "description": "Star Guardian Invasion: Normal",
         "notes": null
     },
     {
         "queueId": 990,
         "map": "Valoran City Park",
-        "description": "Star Guardian Invasion: Onslaught games",
+        "description": "Star Guardian Invasion: Onslaught",
         "notes": null
     },
     {
         "queueId": 1000,
         "map": "Overcharge",
-        "description": "PROJECT: Hunters games",
+        "description": "PROJECT: Hunters",
         "notes": null
     },
     {
         "queueId": 1010,
         "map": "Grieta del invocador",
-        "description": "Snow ARURF games",
+        "description": "Snow ARURF",
         "notes": null
     },
     {
         "queueId": 1020,
         "map": "Grieta del invocador",
-        "description": "One for All games",
+        "description": "One for All",
         "notes": null
     },
     {
         "queueId": 1030,
         "map": "Crash Site",
-        "description": "Odyssey Extraction: Intro games",
+        "description": "Odyssey Extraction: Intro",
         "notes": null
     },
     {
         "queueId": 1040,
         "map": "Crash Site",
-        "description": "Odyssey Extraction: Cadet games",
+        "description": "Odyssey Extraction: Cadet",
         "notes": null
     },
     {
         "queueId": 1050,
         "map": "Crash Site",
-        "description": "Odyssey Extraction: Crewmember games",
+        "description": "Odyssey Extraction: Crewmember",
         "notes": null
     },
     {
         "queueId": 1060,
         "map": "Crash Site",
-        "description": "Odyssey Extraction: Captain games",
+        "description": "Odyssey Extraction: Captain",
         "notes": null
     },
     {
         "queueId": 1070,
         "map": "Crash Site",
-        "description": "Odyssey Extraction: Onslaught games",
+        "description": "Odyssey Extraction: Onslaught",
         "notes": null
     },
     {
         "queueId": 1090,
         "map": "Convergence",
-        "description": "Teamfight Tactics games",
+        "description": "Teamfight Tactics",
         "notes": null
     },
     {
         "queueId": 1100,
         "map": "Convergence",
-        "description": "Ranked Teamfight Tactics games",
+        "description": "Ranked Teamfight Tactics",
         "notes": null
     },
     {
         "queueId": 1110,
         "map": "Convergence",
-        "description": "Teamfight Tactics Tutorial games",
+        "description": "Teamfight Tactics Tutorial",
         "notes": null
     },
     {
         "queueId": 1111,
         "map": "Convergence",
-        "description": "Teamfight Tactics test games",
+        "description": "Teamfight Tactics test",
         "notes": null
     },
     {
         "queueId": 1200,
         "map": "Nexus Blitz",
-        "description": "Nexus Blitz games",
+        "description": "Nexus Blitz",
         "notes": "Deprecated in patch 9.2"
     },
     {
         "queueId": 1300,
         "map": "Nexus Blitz",
-        "description": "Nexus Blitz games",
+        "description": "Nexus Blitz",
         "notes": null
     },
     {
         "queueId": 1400,
         "map": "Grieta del invocador",
-        "description": "Ultimate Spellbook games",
+        "description": "Ultimate Spellbook",
         "notes": null
     },
     {
