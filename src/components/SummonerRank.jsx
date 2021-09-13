511b2ba78972df399af0044dd7e97b800015a0ed
Alberto <info@peralstudio.com>
2021-09-13 17:14:59 +0200

    Update SummonerRank.jsx

--- src/components/SummonerRank.jsx
+++ src/components/SummonerRank.jsx
@@ -10,10 +10,12 @@ import { queueId } from "../dataDragon/queueid";
 const SummonerRank = ({ data, summData, name, allLoad, err, dataLive }) => {
 
     const found = queueId.find(element => element.queueId === dataLive.gameQueueConfigId);
+    const foundRankedSolo = data.find(element => element.queueType === 'RANKED_SOLO_5x5');
+    // const foundRankedFlex = data.find(element => element.queueType === 'RANKED_FLEX_SR');
 
     let winrateColor = "";
 
-    data && ((data.wins / (data.wins + data.losses)) * 100).toFixed(1) < 50
+    foundRankedSolo && ((foundRankedSolo.wins / (foundRankedSolo.wins + foundRankedSolo.losses)) * 100).toFixed(1) < 50
         ? (winrateColor = "winrate-red")
         : (winrateColor = "winrate-green");
 
@@ -21,7 +23,7 @@ const SummonerRank = ({ data, summData, name, allLoad, err, dataLive }) => {
         labels: ["Perdidas", "Ganadas"],
         datasets: [
             {
-                data: [data && data.losses, data && data.wins],
+                data: [foundRankedSolo && foundRankedSolo.losses, foundRankedSolo && foundRankedSolo.wins],
                 backgroundColor: ["#EE5952", "#208ECE"],
                 hoverBackgroundColor: ["#EE5952", "#208ECE"],
                 borderColor: ["#EE5952", "#208ECE"],
@@ -32,86 +34,87 @@ const SummonerRank = ({ data, summData, name, allLoad, err, dataLive }) => {
 
     return (
         <>
-            {allLoad ? <div className="container-data">
-                <div>
-                    <h2 className="card-title">
-                        {data.summonerName}
+            {allLoad ?
+                <div className="container-data">
+                    <div>
+                        <h2 className="card-title">
+                            {foundRankedSolo.summonerName}
+                            <img
+                                src={`https://ddragon.leagueoflegends.com/cdn/11.17.1/img/profileicon/${summData.profileIconId}.png`}
+                                className={validarElo2(foundRankedSolo.tier)}
+                                alt="..."
+                                style={{
+                                    width: "4rem",
+                                    margin: "auto",
+                                    marginLeft: "5px",
+                                    borderRadius: "50%",
+                                }}
+                            />
+                        </h2>
+                    </div>
+                    <img
+                        src={validarElo(foundRankedSolo.tier)}
+                        className="card-img-top"
+                        alt="rank"
+                        style={{ width: "8rem", margin: "auto" }}
+                    />
+                    {!err &&
+                        <p
+                            style={{ backgroundColor: '#EE4142', width: 'fit-content', margin: 'auto', marginTop: '10px', borderRadius: '5px', padding: '0px 5px', fontSize: '14px', fontWeight: 'bold' }}
+                        >
+                            En partida {found && found.description}
+                        </p>
+                    }
+                    <div className="card-body">
+                        <h5 className="card-title">
+                            <span>
+                                Nivel {summData.summonerLevel} -
+                                <span className={`${validarElo2(foundRankedSolo.tier)}-font`}>
+                                    {" "}
+                                    {foundRankedSolo.tier} {foundRankedSolo.rank}
+                                </span>
+                            </span>
+                        </h5>
+                        <p>LP: {foundRankedSolo.leaguePoints}</p>
+                        <div className="card-text">
+                            {/* {data && <p style={{ marginBottom: '0' }}>Jugadas: {data.wins + data.losses}</p>} */}
+                            {/* <p style={{ marginBottom: '0' }}>{data.wins}V {data.losses}L</p> */}
+                            <span>Tasa de Victoria </span>
+                            <span className={winrateColor}>
+                                {(
+                                    (foundRankedSolo.wins / (foundRankedSolo.wins + foundRankedSolo.losses)) *
+                                    100
+                                ).toFixed(1)}
+                                %
+                            </span>
+                            <p>Clasificatoria - {foundRankedSolo.queueType === 'RANKED_SOLO_5x5' ? 'SOLO/DÚO' : '5V5 FLEXIBLE'}</p>
+                        </div>
+                        {/*********** ChartJs **************/}
+
+                        <div className="doughnut">
+                            <Doughnut data={dataChart} />
+                        </div>
+
+                        <Link to={`/histories/${name}`}><button style={{ margin: '20px' }} type="button" className="btn btn-outline-info">Historial</button></Link>
+
+                    </div>
+                    <a
+                        href={`https://euw.op.gg/summoner/userName=${name}`}
+                        target="_blank"
+                        rel="noreferrer"
+                    >
                         <img
-                            src={`https://ddragon.leagueoflegends.com/cdn/11.17.1/img/profileicon/${summData.profileIconId}.png`}
-                            className={validarElo2(data.tier)}
-                            alt="..."
+                            src={opgg}
+                            className="card-img-top"
+                            alt="opgg"
                             style={{
-                                width: "4rem",
+                                width: "10rem",
                                 margin: "auto",
-                                marginLeft: "5px",
-                                borderRadius: "50%",
+                                marginBottom: '20px'
                             }}
                         />
-                    </h2>
-                </div>
-                <img
-                    src={validarElo(data.tier)}
-                    className="card-img-top"
-                    alt="rank"
-                    style={{ width: "8rem", margin: "auto" }}
-                />
-                {!err &&
-                    <p
-                        style={{ backgroundColor: '#EE4142', width: 'fit-content', margin: 'auto', marginTop: '10px', borderRadius: '5px', padding: '0px 5px', fontSize: '14px', fontWeight: 'bold' }}
-                    >
-                        En partida {found && found.description}
-                    </p>
-                }
-                <div className="card-body">
-                    <h5 className="card-title">
-                        <span>
-                            Nivel {summData.summonerLevel} -
-                            <span className={`${validarElo2(data.tier)}-font`}>
-                                {" "}
-                                {data.tier} {data.rank}
-                            </span>
-                        </span>
-                    </h5>
-                    <p>LP: {data.leaguePoints}</p>
-                    <div className="card-text">
-                        {/* {data && <p style={{ marginBottom: '0' }}>Jugadas: {data.wins + data.losses}</p>} */}
-                        {/* <p style={{ marginBottom: '0' }}>{data.wins}V {data.losses}L</p> */}
-                        <span>Tasa de Victoria </span>
-                        <span className={winrateColor}>
-                            {(
-                                (data.wins / (data.wins + data.losses)) *
-                                100
-                            ).toFixed(1)}
-                            %
-                        </span>
-                        <p>{data.queueType}</p>
-                    </div>
-                    {/*********** ChartJs **************/}
-
-                    <div className="doughnut">
-                        <Doughnut data={dataChart} />
-                    </div>
-
-                    <Link to={`/histories/${name}`}><button style={{ margin: '20px' }} type="button" className="btn btn-outline-info">Historial</button></Link>
-
-                </div>
-                <a
-                    href={`https://euw.op.gg/summoner/userName=${name}`}
-                    target="_blank"
-                    rel="noreferrer"
-                >
-                    <img
-                        src={opgg}
-                        className="card-img-top"
-                        alt="opgg"
-                        style={{
-                            width: "10rem",
-                            margin: "auto",
-                            marginBottom: '20px'
-                        }}
-                    />
-                </a>
-            </div >
+                    </a>
+                </div >
                 :
                 <>
                     <Loader
