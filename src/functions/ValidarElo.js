de2213c73817c7da7b7e9d0635503c1698a1e56d
Alberto <info@peralstudio.com>
2022-02-04 19:20:41 +0100

    Add wings ranks

--- src/functions/ValidarElo.js
+++ src/functions/ValidarElo.js
@@ -1,12 +1,12 @@
-import challenger from "../assets/Emblems/Challenger.png";
-import grandmaster from "../assets/Emblems/Grandmaster.png";
-import master from "../assets/Emblems/Master.png";
-import diamond from "../assets/Emblems/Diamond.png";
-import platinum from "../assets/Emblems/Platinum.png";
-import gold from "../assets/Emblems/Gold.png";
-import silver from "../assets/Emblems/Silver.png";
-import bronze from "../assets/Emblems/Bronze.png";
-import iron from "../assets/Emblems/Iron.png";
+import challenger from "../assets/wings/wings_challenger.png";
+import grandmaster from "../assets/wings/wings_grandmaster.png";
+import master from "../assets/wings/wings_master.png";
+import diamond from "../assets/wings/wings_diamond.png";
+import platinum from "../assets/wings/wings_platinum.png";
+import gold from "../assets/wings/wings_gold.png";
+import silver from "../assets/wings/wings_silver.png";
+import bronze from "../assets/wings/wings_bronze.png";
+import iron from "../assets/wings/wings_iron.png";
 
 export const validarElo = (elo) => {
     switch (elo) {
