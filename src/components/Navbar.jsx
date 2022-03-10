fea1f451bb0c59536a637c995530a794fe597da7
Alberto <info@peralstudio.com>
2022-03-10 07:06:20 +0100

    Added new logos

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 import logoNavbar from "../assets/img/logonavbar.png";
+import jhinLogo from "../assets/img/jhin_logo.png";
+import teemoLogo from "../assets/img/teemo-logo.png";
 
 const Navbar = ({
     setSummData,
@@ -17,27 +19,41 @@ const Navbar = ({
     return (
         <nav className="navbar" style={{ backgroundColor: "#0B151C" }}>
             <div className="container justify-content-center">
-                <Link to={`/`}>
+                <Link
+                    to={`/`}
+                    onClick={() => {
+                        setSummData("");
+                        setData("");
+                        setDataFlex("");
+                        setName("");
+                        setHistoryGames("");
+                        setAllLoad(false);
+                        setError(false);
+                        setDataLive("");
+                        setErr(false);
+                    }}
+                >
+                    <img
+                        src={teemoLogo}
+                        alt="logo2"
+                        style={{ width: "3.2rem", marginRight: "10px" }}
+                        className="icon-logo"
+                    />
                     <img
                         href="/"
                         src={logoNavbar}
                         alt="logo"
-                        onClick={() => {
-                            setSummData("");
-                            setData("");
-                            setDataFlex("");
-                            setName("");
-                            setHistoryGames("");
-                            setAllLoad(false);
-                            setError(false);
-                            setDataLive("");
-                            setErr(false);
-                        }}
                         style={{
                             width: "210px",
                             height: "60px",
                             cursor: "pointer",
                         }}
+                    />
+                    <img
+                        src={jhinLogo}
+                        alt="logo3"
+                        style={{ width: "2.2rem", marginLeft: "10px" }}
+                        className="icon-logo2"
                     />
                 </Link>
             </div>
